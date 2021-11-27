1cdf94297f8d3f0f37e0a33d2c4f8fa777ddb9e3
Ignacio Diaz <nachodiaz99@gmail.com>
2021-11-27 11:22:40 +1100

    Init

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const Blog of blogData) {
+    await blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
